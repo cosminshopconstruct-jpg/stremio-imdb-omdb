14d92c305f456f88a7466a5c4f3a5bb0d775b154
cosminshopconstruct-jpg <cosmin.shopconstruct@gmail.com>
2026-01-19 16:25:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const { serveHTTP } = require("stremio-addon-sdk")
-const builder = require("./addon")
+const { serveHTTP } = require("stremio-addon-sdk");
+const builder = require("./addon");
 
 serveHTTP(builder.getInterface(), {
-  port: process.env.PORT || 7000,
-  host: "0.0.0.0"
-})
+  port: process.env.PORT || 7000
+});
